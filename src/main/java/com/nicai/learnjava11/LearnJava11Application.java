a000d4c0dda3f1a9961a8af6378cd96a27fd84f8
guozhe <guozhe513@hotmail.com>
2020-12-13 13:17:23 +0800

    first commit

---
+++ src/main/java/com/nicai/learnjava11/LearnJava11Application.java
@@ -0,0 +1,13 @@
+package com.nicai.learnjava11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJava11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJava11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicai/learnjava11/LearnJava11ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nicai.learnjava11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJava11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
